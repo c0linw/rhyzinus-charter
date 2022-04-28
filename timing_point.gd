7fd7b124ec7c5c84c57de1be5be39e740d557092
c0linw <41310013+c0linw@users.noreply.github.com>
2022-04-27 23:17:08 -0700

    Implement basic timing point add/remove functionality

--- timing_point.gd
+++ timing_point.gd
@@ -1,10 +1,17 @@
-extends Control
+extends ColorRect
 class_name TimingPoint
 
 var time: float
 var type: String
-var bpm_color: Color = Color(1,0,0,0)
-var velocity_color: Color = Color(0,1,0,0)
+
+# values related to bpm timing point
+var beat_length: float
+var meter: int
+var bpm_color: Color = Color(1,0,0,1)
+
+# values related to velocity timing point
+var velocity: float
+var velocity_color: Color = Color(0,1,0,1)
 
 signal custom_gui_input(event, timingpoint)
 
@@ -13,8 +20,17 @@ func _ready():
 	pass # Replace with function body.
 
 func set_data(timingpoint_data: Dictionary):
-	pass
-
+	time = timingpoint_data.time
+	type = timingpoint_data.type
+	match type:
+		"bpm":
+			beat_length = timingpoint_data.beat_length
+			meter = timingpoint_data.meter
+			color = bpm_color
+		"velocity":
+			velocity = timingpoint_data.velocity
+			color = velocity_color
+			
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
