6c745f047179bcda7432db9a7b2256fc2366c5b3
c0linw <41310013+c0linw@users.noreply.github.com>
2022-06-14 20:10:46 -0700

    Fix timestamp formatting

--- playback_time_label.gd
+++ playback_time_label.gd
@@ -18,8 +18,8 @@ func _ready():
 
 func _on_SongAudioPlayer_song_position_updated(new_position, max_position):
 	var new_whole = int(new_position)
-	var new_decimal = str(new_position - new_whole).trim_prefix("0.").left(3)
+	var new_decimal = ("%.03f" % (new_position - new_whole)).trim_prefix("0.")
 	
 	var max_whole = int(max_position)
-	var max_decimal = str(max_position - max_whole).trim_prefix("0.").left(3)
+	var max_decimal = ("%.03f" % (max_position - max_whole)).trim_prefix("0.")
 	text = "%s:%02d.%s / %s:%02d.%s" % [new_whole/60, new_whole%60, new_decimal, max_whole/60, max_whole%60, max_decimal]
